e1143a0f01b0e08d4d50450851327ffe420f9023
Arsine <arsine@superannotate.com>
2020-08-22 21:46:37 +0000

    add inference logging

--- autoqa/detect_noise.py
+++ autoqa/detect_noise.py
@@ -2,7 +2,8 @@ from detectron2.data import MetadataCatalog, build_detection_test_loader
 from detectron2.modeling import build_model
 from detectron2.checkpoint import DetectionCheckpointer
 from detectron2.structures import pairwise_iou
-
+from detectron2.utils.logger import log_every_n_seconds
+from detectron2.utils.comm import get_world_size
 
 from .utils.loader import build_test_loader
 from .utils.ao_utils import ao_setup_project_dir, ao_format_annotations
@@ -12,6 +13,9 @@ from torch.nn import functional as F
 import numpy as np
 import torch
 import copy
+import logging
+import datetime
+import time
 
 def add_augmentations(inputs):
 
@@ -75,6 +79,8 @@ def detect_mislabeled_annotations_per_image(inputs, model, augment = False, iou_
 
     for aug_idx, predictions in enumerate(outputs):
         pred_boxes, pred_scores = get_pred_data(predictions)
+        if len(pred_boxes) == 0 or len(gt_boxes) == 0:
+            continue
         matched_gt_ids, matched_pred_ids = match_gt_to_pred(gt_boxes, pred_boxes, iou_thresh)
         
         for gt_idx, pred_idx in zip(matched_gt_ids, matched_pred_ids):
@@ -98,13 +104,65 @@ def detect_mislabeled_annotations(dataset_name, cfg, mismatch_thresh = 0.4, augm
     DetectionCheckpointer(model).load(cfg.MODEL.WEIGHTS)
     model.eval()
 
+    n = len(data_loader)
+    num_devices = get_world_size()
+    logger = logging.getLogger(__name__)
+    logger.info(
+        "Start mislabel detection on {} images".format(n)
+    )
+    
+    num_warmup = min(5, n - 1)
+    start_time = time.perf_counter()
+    total_compute_time = 0
+
+
     for inputs in data_loader:
+        
+        if idx == num_warmup:
+            start_time = time.perf_counter()
+            total_compute_time = 0
+
+        start_compute_time = time.perf_counter()
+
         gt_scores, gt_classes, gt_boxes, _ = detect_mislabeled_annotations_per_image(inputs, model, augment)
         mislabeled_gt_inds = gt_scores > mismatch_thresh
        
         gt_class_info = [(class_id.item(), class_names[class_id]) for class_id in gt_classes]
         ao_format_annotations(inputs[0]['image_id'], ao_json_dir, gt_boxes, gt_class_info, mislabeled_gt_inds)
      
+        total_compute_time += time.perf_counter() - start_compute_time
+        iters_after_start = idx + 1 - num_warmup * int(idx >= num_warmup)
+        seconds_per_img = total_compute_time / iters_after_start
+        if idx >= num_warmup * 2 or seconds_per_img > 5:
+            total_seconds_per_img = (
+                time.perf_counter() - start_time
+            ) / iters_after_start
+            eta = datetime.timedelta(
+                seconds=int(total_seconds_per_img * (n - idx - 1))
+            )
+            log_every_n_seconds(
+                logging.INFO,
+                "Proessed {}/{}. {:.4f} s / img. ETA={}".format(
+                    idx + 1, n, seconds_per_img, str(eta)
+                ),
+                n=5,
+            )
+    # Measure the time only for this worker (before the synchronization barrier)
+    total_time = time.perf_counter() - start_time
+    total_time_str = str(datetime.timedelta(seconds=total_time))
+    
+    logger.info(
+        "Total detection time: {} ({:.6f} s / img per device, on {} devices)".format(
+            total_time_str, total_time / (n - num_warmup), num_devices
+        )
+    )
+    total_compute_time_str = str(datetime.timedelta(seconds=int(total_compute_time)))
+    logger.info(
+        "Total detection pure compute time: {} ({:.6f} s / img per device, on {} devices)".format(
+            total_compute_time_str, total_compute_time / (n - num_warmup), num_devices
+        )
+    )
+
 def eval_mislabel_detection(dataset_name, cfg, mismatch_thresh = 0.4, augment =False):
 
     data_loader = build_test_loader(cfg, dataset_name)
@@ -120,30 +178,67 @@ def eval_mislabel_detection(dataset_name, cfg, mismatch_thresh = 0.4, augment =F
     total = torch.zeros(n)
     npos = torch.zeros(n)
 
+    num_devices = get_world_size()
+    logger = logging.getLogger(__name__)
+    logger.info(
+        "Start mislabel evaluation on {} images".format(n)
+    )
+    
+    num_warmup = min(5, n - 1)
+    start_time = time.perf_counter()
+    total_compute_time = 0
+
     for idx, inputs in enumerate(data_loader):
+        if idx == num_warmup:
+            start_time = time.perf_counter()
+            total_compute_time = 0
+
+        start_compute_time = time.perf_counter()
+        
         gt_scores, _, _, gt_mislabeled_ids = detect_mislabeled_annotations_per_image(inputs, model, augment)
         pred_mislabeled_ids = gt_scores > mismatch_thresh
-        
+
         total[idx] = gt_scores.shape[0]
         npos[idx] = torch.sum(gt_mislabeled_ids).int()
 
         tp[idx] = torch.sum(torch.logical_and(gt_mislabeled_ids, pred_mislabeled_ids)).int()
         fp[idx] = torch.sum(torch.logical_and(torch.logical_not(gt_mislabeled_ids), pred_mislabeled_ids)).int()
-
+        
+        total_compute_time += time.perf_counter() - start_compute_time
+        iters_after_start = idx + 1 - num_warmup * int(idx >= num_warmup)
+        seconds_per_img = total_compute_time / iters_after_start
+        if idx >= num_warmup * 2 or seconds_per_img > 5:
+            total_seconds_per_img = (
+                time.perf_counter() - start_time
+            ) / iters_after_start
+            eta = datetime.timedelta(
+                seconds=int(total_seconds_per_img * (n - idx - 1))
+            )
+            log_every_n_seconds(
+                logging.INFO,
+                "Processed {}/{}. {:.4f} s / img. ETA={}".format(
+                    idx + 1, n, seconds_per_img, str(eta)
+                ),
+                n=5,
+            )
+            
     recall = torch.sum(tp)/ torch.sum(npos)
     precision = torch.sum(tp)/ (torch.sum(tp) + torch.sum(fp))
     qa = (torch.sum(tp) + torch.sum(fp))/torch.sum(total)
+
+    # Measure the time only for this worker (before the synchronization barrier)
+    total_time = time.perf_counter() - start_time
+    total_time_str = str(datetime.timedelta(seconds=total_time))
+    
+    logger.info(
+        "Total evaluation time: {} ({:.6f} s / img per device, on {} devices)".format(
+            total_time_str, total_time / (n - num_warmup), num_devices
+        )
+    )
+    total_compute_time_str = str(datetime.timedelta(seconds=int(total_compute_time)))
+    logger.info(
+        "Total evaluation pure compute time: {} ({:.6f} s / img per device, on {} devices)".format(
+            total_compute_time_str, total_compute_time / (n - num_warmup), num_devices
+        )
+    )
     return recall.item(), precision.item(), qa.item()
-    
-    
-
-    
-    
-
-
-        
-    
-    
-
-
-
